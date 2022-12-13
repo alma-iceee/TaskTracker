abf6de5e99c4fc47058e8482935b83222ee4e21f
alma-iceee <alma.iceee@gmail.com>
2022-12-13 21:19:49 +0600

    Add DbContext

---
+++ TaskTracker/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTracker.Models;
+
+namespace TaskTracker
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
