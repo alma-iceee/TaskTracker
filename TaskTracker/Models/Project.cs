12522972b9c7f9f4d4473976a1ae37207347ce99
alma-iceee <alma.iceee@gmail.com>
2022-12-13 21:18:31 +0600

    Update models

--- TaskTracker/Models/Project.cs
+++ TaskTracker/Models/Project.cs
@@ -6,9 +6,9 @@ namespace TaskTracker.Models
     {
         public long Id { get; set; }
         public string? Name { get; set; }
-        public DateTime StartDate { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime CompletionDate { get; set; }
-        public ProjectStatus Status { get; set; }
+        public ProjectStatus Status { get; set; } = ProjectStatus.NotStarted;
         public int Priority { get; set; }
     }
 }
--- TaskTracker/Models/Task.cs
+++ TaskTracker/Models/Task.cs
@@ -1,11 +1,17 @@
-﻿namespace TaskTracker.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TaskTracker.Models.Enums;
+
+namespace TaskTracker.Models
 {
     public class Task
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public TaskStatus Status { get; set; }
+        public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.ToDo;
         public string? Description { get; set; }
         public int Priority { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project? Project { get; set; }
     }
 }
