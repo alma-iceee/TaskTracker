ba2e67b9274955b8551057bdb4e76094964fd699
alma-iceee <alma.iceee@gmail.com>
2022-12-15 17:38:38 +0600

    Added attributes to models

--- TaskTracker/Models/Project.cs
+++ TaskTracker/Models/Project.cs
@@ -1,14 +1,17 @@
-﻿using TaskTracker.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskTracker.Models.Enums;
 
 namespace TaskTracker.Models
 {
     public class Project
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
-        public DateTime StartDate { get; set; } = DateTime.Now;
-        public DateTime CompletionDate { get; set; }
-        public ProjectStatus Status { get; set; } = ProjectStatus.NotStarted;
+        public DateTime? StartDate { get; set; } = DateTime.Now;
+        public DateTime? CompletionDate { get; set; }
+        public ProjectStatus? Status { get; set; } = ProjectStatus.NotStarted;
+        [Required]
         public int Priority { get; set; }
     }
 }
--- TaskTracker/Models/Task.cs
+++ TaskTracker/Models/Task.cs
@@ -1,16 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using TaskTracker.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskTracker.Models
 {
     public class Task
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
-        public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.ToDo;
+        public Enums.TaskStatus? Status { get; set; } = Enums.TaskStatus.ToDo;
         public string? Description { get; set; }
+        [Required]
         public int Priority { get; set; }
-
+        public int ProjectId { get; set; }
         [ForeignKey("ProjectId")]
         public Project? Project { get; set; }
     }
